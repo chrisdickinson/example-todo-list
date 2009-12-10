aa12a70ff74782f7af17d62d91d81667bc61bec1
Chris Dickinson <christopher.s.dickinson@gmail.com>
2009-12-10 16:38:54 -0600

    fixed slug regex in todo/urls.py. sorry annie\!

--- todo/urls.py
+++ todo/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls.defaults import patterns, include, handler500, url
 urlpatterns = patterns(
     'todo.views',
     url(r'^$', 'todo_list_all', name='todo-list-all'),
-    url(r'^(?P<slug>[a-zA-Z0-9\-_])/$', 'todo_list', name='todo-list-detail'),
-    url(r'^(?P<slug>[a-zA-Z0-9\-_])/(?P<pk>\d+)/$', 'todo_list_item', name='todo-list-item'),
+    url(r'^(?P<slug>[a-zA-Z0-9\-_]+)/$', 'todo_list', name='todo-list-detail'),
+    url(r'^(?P<slug>[a-zA-Z0-9\-_]+)/(?P<pk>\d+)/$', 'todo_list_item', name='todo-list-item'),
 )
