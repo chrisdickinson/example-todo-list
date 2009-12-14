75a87ba4c77d604fceae3d8ef605e462062fae70
Chris Dickinson <christopher.s.dickinson@gmail.com>
2009-12-14 15:30:15 -0600

    another mistake. hard to check these things in such a rapid fashion.

--- todo/views.py
+++ todo/views.py
@@ -29,6 +29,7 @@ def todo_list(request, slug):
 def todo_list_item(request, slug, pk):
     list_item = get_object_or_404(Item, list__slug=slug, pk=int(pk))
 
+    completion_form = None
     if request.user.is_authenticated():
         completion_form = CompletionForm({'user':request.user.pk, 'is_done':list_item.is_done})
         if request.method == 'POST':
